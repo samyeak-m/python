5ea8ae930ad4e64cd2ceeb52b6a436c16546ba19
samyeak-m <maharjansamyak@gmail.com>
2023-12-03 09:19:35 +0545

    Initial commit

---
+++ firstclass.py
@@ -0,0 +1,11 @@
+print("hello")
+first_name="Samyak"
+last_name="Maharjan"
+print(first_name)
+print(last_name)
+print(first_name,last_name)
+print(first_name+last_name)
+
+x = str(3) #'3'
+y = int(3) #3
+z = float(3) #3.0
